8a97eef91ddc598550dc5739f72059dcb9c1470e
Sergei Kim <sergei.kim@wayray.com>
2021-04-19 09:19:47 +0300

    foundation

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def main():
             "License :: OSI Approved :: MIT License",
             "Operating System :: OS Independent",
         ],
-        python_requires='>=3.7',
+        python_requires='>=3.8',
         install_requires=reqs,
     )
 
